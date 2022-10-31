8066c23af7c59ee44c3dceb3ea99dd2e835504bd
Austin Milt <austin@holaplex.com>
2022-10-30 18:02:33 -0700

    subscribe to player state

--- app/src/components/shared/game/provider.tsx
+++ app/src/components/shared/game/provider.tsx
@@ -1,8 +1,16 @@
 import { PublicKey } from "@solana/web3.js";
-import { createContext, ReactNode, useContext, useState } from "react";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { useEquilibrate } from "../../../lib/equilibrate/provider";
+import { PlayerStateEvent } from "../../../lib/equilibrate/sdk";
+import { PlayerState } from "../../../lib/equilibrate/types";
 
 export interface ActiveGameContextState {
     address: PublicKey | undefined;
+
+    /**
+     * Player's state account in the active game. Will be `null` if they are not in the game.
+     */
+    playerState: PlayerState | null;
     set: (address: PublicKey | undefined) => void;
 }
 
@@ -19,10 +27,62 @@ export function useActiveGame(): ActiveGameContextState {
 
 export function ActiveGameProvider(props: { children: ReactNode }): JSX.Element {
     const [activeGame, setActiveGame] = useState<ActiveGameContextState["address"]>();
+    const { player } = useEquilibrate();
+    const { playerState } = usePlayerState(player, activeGame);
 
     return (
-        <ActiveGameContext.Provider value={{address: activeGame, set: setActiveGame}}>
+        <ActiveGameContext.Provider value={{
+            address: activeGame,
+            playerState: playerState,
+            set: setActiveGame,
+        }}>
             {props.children}
         </ActiveGameContext.Provider>
     );
+}
+
+
+interface PlayerStateContext {
+    playerState: PlayerState | null;
+    loading: boolean;
+    error: Error | undefined;
+}
+
+
+function usePlayerState(playerAddress: PublicKey | undefined, gameAddress: PublicKey | undefined): PlayerStateContext {
+    const { equilibrate, equilibrateIsReady } = useEquilibrate();
+    const [playerState, setPlayerState] = useState<PlayerState | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<Error | undefined>();
+
+    // (un)subscribe to the current player state
+    useEffect(() => {
+        const run = async () => {
+            if (equilibrateIsReady && (gameAddress !== undefined) && (playerAddress !== undefined)) {
+                equilibrate.watchPlayer(
+                    playerAddress,
+                    gameAddress,
+                    (event: PlayerStateEvent) => setPlayerState(event.player),
+                    true
+                );
+
+                return () => {
+                    // TODO this is an async method so it may return after the component is unmounted :(
+                    // TODO likely the cause of "WebSocket is already in CLOSING or CLOSED state."
+                    equilibrate.stopWatchingPlayerState(playerAddress, gameAddress);
+                };
+            }
+        };
+
+        setError(undefined);
+        setLoading(true);
+        run().catch(setError).finally(() => setLoading(false));
+    }, [gameAddress, equilibrateIsReady]);
+
+
+    return {
+        playerState: playerState,
+        loading: loading,
+        error: error
+    };
 }
