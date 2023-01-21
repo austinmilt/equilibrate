aeb13973a03dc0d09287d8c8cc9521d3b07d92b3
Austin Milt <austin@holaplex.com>
2023-01-21 12:38:47 -0500

    update sdk provider to allow setting an override payer

--- app/src/lib/equilibrate/provider.tsx
+++ app/src/lib/equilibrate/provider.tsx
@@ -3,6 +3,7 @@ import {
     ReactNode,
     useContext,
     useEffect,
+    useMemo,
     useState,
 } from "react";
 import * as anchor from "@project-serum/anchor";
@@ -15,7 +16,17 @@ import { PublicKey } from "@solana/web3.js";
 export interface EquilibrateProgramContextState {
     equilibrate: EquilibrateSDK;
     equilibrateIsReady: boolean;
+
+    /**
+     * Game player. Will always be the one provided by the connected wallet.
+     */
     player: PublicKey | undefined;
+
+    /**
+     * @param payer payer to use in place of wallet from wallet-adapter (e.g. autosign payer)
+     * @returns void
+     */
+    setPayer: (payer: anchor.Wallet) => void;
 }
 
 export const EquilibrateProgramContext = createContext<EquilibrateProgramContextState>(
@@ -28,14 +39,21 @@ export function useEquilibrate(): EquilibrateProgramContextState {
 
 export function EquilibrateProgramProvider(props: { children: ReactNode }): JSX.Element {
     const [sdk, setSdk] = useState<EquilibrateSDK>(EquilibrateSDK.dummy());
+    const [overridePayer, setOverridePayer] = useState<anchor.Wallet | undefined>();
     const anchorWallet = useAnchorWallet();
     const { connection } = useConnection();
+
+    const payer: anchor.Wallet = useMemo(() => {
+        if (overridePayer !== undefined) return overridePayer;
+        if (anchorWallet != null) return (anchorWallet as anchor.Wallet);
+        // fallback value allows querying the program without having a wallet connected
+        return ({} as anchor.Wallet);
+    }, [overridePayer, anchorWallet]);
 
     useEffect(() => {
         const provider: anchor.AnchorProvider = new anchor.AnchorProvider(
             connection,
-            // fallback value allows querying the program without having a wallet connected
-            anchorWallet ?? ({} as anchor.Wallet),
+            payer,
             anchor.AnchorProvider.defaultOptions()
         );
         const program: anchor.Program<Equilibrate> = new anchor.Program(
@@ -44,12 +62,13 @@ export function EquilibrateProgramProvider(props: { children: ReactNode }): JSX.
             provider ?? ({} as anchor.AnchorProvider)
         );
         setSdk(EquilibrateSDK.from(program));
-    }, [anchorWallet, connection, setSdk]);
+    }, [payer, connection, setSdk]);
 
     const value: EquilibrateProgramContextState = {
         equilibrate: sdk,
         equilibrateIsReady: sdk.isReady(),
-        player: anchorWallet?.publicKey
+        player: anchorWallet?.publicKey,
+        setPayer: setOverridePayer
     };
 
     return (
